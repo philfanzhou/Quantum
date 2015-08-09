b19ca8bd5d8528126b0a360d4dc68f73095627d3
philfanzhou <philfanzhou@hotmail.com>
2015-08-09 15:20:48 +0800

    add config files

---
+++ 5_Infrastructure/Quantum.Infrastructure.EntityFramework.Configuration/DatabaseConfig/QuantumDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Framework.Infrastructure.Repository.EntityFramework;
+using System.Data.Entity;
+
+namespace Quantum.Infrastructure.EntityFramework.Configuration
+{
+    public sealed class QuantumDbContext : SqlServerDbContext
+    {
+        private static readonly ConnectionConfig sqlServer = new ConnectionConfig
+        {
+            Server = @"(localdb)\projects",
+            Database = "Trading",
+            TrustedConnection = true
+        };
+
+        private static readonly ConnectionConfig mySql = new ConnectionConfig
+        {
+            Server = "localhost",
+            Port = "3306",
+            Database = "UserContextDb",
+            Uid = "root",
+            Password = "123456"
+        };
+
+        public QuantumDbContext()
+            //: base(mySql)
+            : base(sqlServer)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AccountDataConfig());
+        }
+    }
+}
