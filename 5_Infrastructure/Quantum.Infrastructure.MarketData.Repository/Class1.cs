9a53b105bc1176cf7b097bf193e152e48fb5a06d
philfanzhou <philfanzhou@hotmail.com>
2015-09-08 23:00:08 +0800

    Add memory mapped file

---
+++ 5_Infrastructure/Quantum.Infrastructure.MarketData.Repository/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Infrastructure.MarketData.Repository
+{
+    class Class1
+    {
+        public void fun1()
+        {
+            //MemoryMappedFile mmf = MemoryMappedFile.CreateOrOpen();
+
+            throw new NotImplementedException();
+        }
+    }
+}
