aa34b9439651423a670a8d3b3315ae37b49a7480
philfanzhou <philfanzhou@hotmail.com>
2016-02-03 17:55:32 +0800

    修改多空比接口

---
+++ 4_Domain/Quantum.Domain.MarketData/Entity/BuyAgainstSell.cs
@@ -0,0 +1,47 @@
+﻿using Ore.Infrastructure.MarketData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Domain.MarketData.Entity
+{
+    internal class BuyAgainstSell : IBuyAgainstSell
+    {
+        public BuyAgainstSell(IStockIntraday stockIntraday)
+        {
+
+        }
+
+        public DateTime Time { get; private set; }
+
+        public double BuyVolume { get; private set; }
+
+        public double SellVolume { get; private set; }
+
+        public double CommissionDiff
+        {
+            get
+            {
+                return BuyVolume - SellVolume;
+            }
+        }
+
+        public double CommissionRatio
+        {
+            get
+            {
+                return Math.Round(CommissionDiff / (BuyVolume + SellVolume) * 100, 2);
+            }
+        }
+
+        public double MultiEmptyRatio
+        {
+            get
+            {
+                return Math.Round(BuyVolume / SellVolume, 2);
+            }
+        }
+    }
+}
--- 4_Domain/Quantum.Domain.MarketData/Indicator/IBuyAgainstSell.cs
+++ 4_Domain/Quantum.Domain.MarketData/Indicator/IBuyAgainstSell.cs
@@ -1,13 +1,40 @@
-﻿
-namespace Quantum.Domain.Indicator
+﻿using System;
+
+namespace Quantum.Domain.MarketData
 {
     /// <summary>
     /// 多空指标
     /// </summary>
     public interface IBuyAgainstSell
     {
+        /// <summary>
+        /// 当前数据的时间
+        /// </summary>
+        DateTime Time { get; }
+
+        /// <summary>
+        /// 委卖
+        /// </summary>
         double SellVolume { get; }
 
+        /// <summary>
+        /// 委买
+        /// </summary>
         double BuyVolume { get; }
+
+        /// <summary>
+        /// 委差
+        /// </summary>
+        double CommissionDiff { get; }
+
+        /// <summary>
+        /// 委比
+        /// </summary>
+        double CommissionRatio { get; }
+
+        /// <summary>
+        /// 多空比
+        /// </summary>
+        double MultiEmptyRatio { get; }
     }
 }
