89f706457ae35afd292925b658c1d0833270894b
philfanzhou <philfanzhou@hotmail.com>
2016-02-04 14:25:39 +0800

    添加时间序列包的实现

---
+++ 4_Domain/Quantum.Domain.MarketData/Entity/TimeSeriesPackage.cs
@@ -0,0 +1,74 @@
+﻿using Ore.Infrastructure.MarketData;
+using System;
+using System.Collections.Generic;
+
+namespace Quantum.Domain.MarketData
+{
+    /// <summary>
+    /// 时间序列数据包裹
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class TimeSeriesPackage<T> : ITimeSeriesPackage<T>
+        where T : ITimeSeries
+    {
+        #region Field
+        private readonly DateTime _startTime;
+        private readonly DateTime _endTime;
+
+        private List<T> _items = new List<T>();
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// 构造时间序列package
+        /// </summary>
+        /// <exception cref="System.ArgumentOutOfRangeException"></exception>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        public TimeSeriesPackage(DateTime startTime, DateTime endTime)
+        {
+            if(startTime > endTime)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            _startTime = startTime;
+            _endTime = endTime;
+        }
+        #endregion
+
+        #region Property
+        DateTime ITimeSeriesPackage<T>.StartTime { get { return _startTime; } }
+
+        DateTime ITimeSeriesPackage<T>.EndTime { get { return _endTime; } }
+
+        IEnumerable<T> ITimeSeriesPackage<T>.Items { get { return _items; } }
+        #endregion
+
+        #region Public Method
+        bool ITimeSeriesPackage<T>.ContainsTime(DateTime time)
+        {
+            return (_startTime < time) && (time < _endTime);
+        }
+
+        void ITimeSeriesPackage<T>.Add(T item)
+        {
+            if(!(this as ITimeSeriesPackage<T>).TryAdd(item))
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        bool ITimeSeriesPackage<T>.TryAdd(T item)
+        {
+            if (!(this as ITimeSeriesPackage<T>).ContainsTime(item.Time))
+            {
+                return false;
+            }
+
+            _items.Add(item);
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ 4_Domain/Quantum.Domain.MarketData/Interface/ITimeSeriesPackage.cs
@@ -0,0 +1,50 @@
+﻿using Ore.Infrastructure.MarketData;
+using System;
+using System.Collections.Generic;
+
+namespace Quantum.Domain.MarketData
+{
+    /// <summary>
+    /// 时间序列数据包裹定义
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface ITimeSeriesPackage<T>
+        where T : ITimeSeries
+    {
+        /// <summary>
+        /// 获取Package管理数据的起始时间
+        /// </summary>
+        DateTime StartTime { get; }
+
+        /// <summary>
+        /// 获取Package管理数据的结束时间
+        /// </summary>
+        DateTime EndTime { get; }
+
+        /// <summary>
+        /// 获取package内的所有Item
+        /// </summary>
+        IEnumerable<T> Items { get; }
+
+        /// <summary>
+        /// 判断指定时间是否在当前的管理范围内
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        bool ContainsTime(DateTime time);
+
+        /// <summary>
+        /// 将数据添加到package中
+        /// </summary>
+        /// <exception cref="System.InvalidOperationException">如果当前package无法将数据加入，则抛出异常</exception>
+        /// <param name="item"></param>
+        void Add(T item);
+
+        /// <summary>
+        /// 将数据添加到package中
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>添加成功返回true</returns>
+        bool TryAdd(T item);
+    }
+}
