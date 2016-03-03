58ab53a6b42785ebdb2014f3e878df55c2008836
philfanzhou <philfanzhou@hotmail.com>
2016-03-03 11:15:33 +0800

    完成调用指标数据的结构

---
+++ 4_Domain/Quantum.Domain.MarketData/Extension/StockKLineExt.cs
@@ -0,0 +1,51 @@
+﻿using Ore.Infrastructure.MarketData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Domain.MarketData
+{
+    /// <summary>
+    /// K线上各种指标数据的扩展
+    /// </summary>
+    public static class StockKLineExt
+    {
+        /// <summary>
+        /// 获取KDJ指标
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static IEnumerable<IKDJ> KDJ(this IEnumerable<IStockKLine> self)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 获取MACD指标
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static IEnumerable<IMACD> MACD(this IEnumerable<IStockKLine> self)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 获取MA指标
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="cycle">均线周期，如：5日均线，10日均线， 20日均线， 60日均线</param>
+        /// <returns></returns>
+        public static IEnumerable<IMA> MA(this IEnumerable<IStockKLine> self, int cycle)
+        {
+            if(cycle <= 1)
+            {
+                throw new ArgumentOutOfRangeException("cycle");
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 6_Test/Test.Domain.MarketData/TestIndication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Domain.MarketData
+{
+    [TestClass]
+    public class TestIndication
+    {
+        [TestMethod]
+        public void TestMACD()
+        {
+            // 从Pitman获取K线数据
+
+            // 调用K线扩展方法获取MACD指标的值
+
+            // 判断值的正确性
+        }
+    }
+}
