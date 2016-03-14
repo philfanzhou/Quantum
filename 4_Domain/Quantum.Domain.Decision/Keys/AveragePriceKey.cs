1575b323bcce8fcfdd4924fe80c8bf760b276b67
philfanzhou <philfanzhou@hotmail.com>
2016-03-14 22:30:55 +0800

    添加Key描述文件

--- 4_Domain/Quantum.Domain.Decision/Keys/AveragePriceKey.cs
+++ 4_Domain/Quantum.Domain.Decision/Keys/AveragePriceKey.cs
@@ -6,19 +6,24 @@ using System.Runtime.Serialization;
 
 namespace Quantum.Domain.Decision.Keys
 {
+    /// <summary>
+    /// 根据1分钟K线上的平均价格线来进行交易决策
+    /// </summary>
     [Serializable]
     public class AveragePriceKey : IKey
     {
         private double _buyArgs;
         private double _sellArgs;
 
-        public AveragePriceKey(double buyArgs, double sellArgs)
+        public AveragePriceKey(double flag)
         {
             _buyArgs = buyArgs;
             _sellArgs = sellArgs;
         }
 
         public KLineType DataType { get { return KLineType.Min1; } }
+
+        internal double Step { get { return 1; } }
 
         /// <summary>
         /// 均价指标判断，不需要数据具有提前量
---
+++ 4_Domain/Quantum.Domain.Decision/Keys/KeyDescription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Domain.Decision.Keys
+{
+    /// <summary>
+    /// Key的描述文件
+    /// </summary>
+    public class KeyDescription
+    {
+        public string Name { get; private set; }
+    }
+}
